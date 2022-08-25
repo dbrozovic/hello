6aa1be66d687260fb8458aa79d0b514938b1ec45
dbrozovic <dbrozovic2@huskers.unl.edu>
2022-08-25 17:07:50 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Daniel Brozović
+ * Date: 2022/08/25
+ *
+ * A simple program to print my name
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	
+	printf("My name is Daniel Brozović!\n");
+
+	return 0;
+
+}
+
